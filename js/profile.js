68bb6f26203f7792b36b931afddb9aefd099374a
Saran408 <saran2002msd&gmail.com>
2023-03-13 15:22:44 +0530

    Everything updated

--- js/profile.js
+++ js/profile.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	
+	alert('we');
 	// get the current user's username from local storage
 	var session_key = localStorage.getItem('session_key');
 	//alert(session_key);
